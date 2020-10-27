90030071b2ae5c970ee2dab30140a1ca0c6b556e
muttul58 <muttul58@naver.com>
2020-10-27 11:54:32 +0900

    EB1_E9 학습완료

---
+++ 골드메탈/BE1_B9/Assets/Remark.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // [유니티 기초 - B9] 실제와 같은 물체를 만들기
+    // Rigidbody : 중력 적용
+    // Is Kinematic : 장해물 중력 적용 안됨
+
+    // Default-Material : 제질
+
+    SphereCollider : 
+}
