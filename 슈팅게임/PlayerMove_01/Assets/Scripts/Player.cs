33e6b345790c1ea76399c1e59176ad3bfa333154
muttul <muttul58@naver.com>
2020-10-30 01:09:41 +0900

    슈팅게임 총알준비

---
+++ 슈팅게임/PlayerMove_01/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 10;
+
+
+    void Update()
+    {
+        // 플레이어 이동
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector3 curPos = transform.position;
+        Vector3 nextPos = new Vector3(h, v, 0) * speed * Time.deltaTime;
+
+        transform.position = curPos + nextPos;
+
+    }
+}
